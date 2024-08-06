b73b24fc6e37a3a29b42308967e010d85c0b6ace
KeeyanGhoreshi <keeyan.ghoreshi@uconn.edu>
2024-08-06 01:08:07 -0400

    reapply

--- src/hooks/hookProxy.ts
+++ src/hooks/hookProxy.ts
@@ -3,14 +3,14 @@ import { Coding } from "fhir/r4";
 import { Connection } from "../lib/schemas/Phonebook";
 
 enum EHRWhitelist {
-  any = 'any', // wildcard, accept anything
-  testEhr = 'http://localhost:8080/test-ehr/r4'
+    any = 'any', // wildcard, accept anything
+    testEhr = 'http://localhost:8080/test-ehr/r4'
 }
 
 const REMSAdminWhitelist = {
-  standardRemsAdmin: config?.general?.remsAdminHookPath,
-  example: 'http://localhost:example'
-};
+    standardRemsAdmin: config?.general?.remsAdminHookPath,
+    example: 'http://localhost:example'
+}
 
 const phonebook = [
     {
@@ -84,36 +84,4 @@ export async function getServiceUrl(coding: Coding, requester: string | undefine
             return connection.to;
         }
     }
-    const connMap = connections
-      .map(conn => {
-        let isAny = false;
-        const filteredConns = conn.from.filter(registeredRequester => {
-          if (registeredRequester === EHRWhitelist.any) {
-            isAny = true;
-            return true;
-          } else if (registeredRequester === requester) {
-            return true;
-          }
-        });
-        if (filteredConns.length > 0) {
-          // found a match
-          return {
-            isAny: isAny,
-            conn: conn
-          };
-        }
-      })
-      .filter(e => {
-        return e;
-      });
-    if (connMap.length > 0) {
-      // we have somewhere to send it
-      for (const result of connMap) {
-        if (result && !result.isAny) {
-          return result.conn.to;
-        }
-      }
-      return connMap[0]?.conn.to; // return our first result, which must be an 'any'
-    }
-  }
-};
+}
